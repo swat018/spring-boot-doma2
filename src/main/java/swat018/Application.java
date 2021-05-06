04a94ff847f6e9c38f5b686cef0f247feb944649
jinwoo park <jwpark@gaia3d.com>
2021-05-06 19:19:03 +0900

    메인 애플리케이션 클래스

---
+++ src/main/java/swat018/Application.java
@@ -0,0 +1,22 @@
+package swat018;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+
+// @Configuration, @EnableAutoConfiguration, @ComponentScan을 지정한 것과 동일
+//@SpringBootApplication(scanBasePackageClasses = {ComponetScanBasePackage.class })
+@EnableAutoConfiguration
+@RestController // 원래 컨트롤러에 작성할 애너테이션
+public class Application {
+    @RequestMapping("/") // 원래 컨트롤러에 작성할 메소드
+    public String hello() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
